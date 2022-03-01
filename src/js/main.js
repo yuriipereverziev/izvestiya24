c3331c269070ac88208f90b05824faaf52fcbe8e
yuriipereverziev <yuripereverzev@gmail.com>
2022-03-01 18:57:04 +0200

    add social + fix

--- src/js/main.js
+++ src/js/main.js
@@ -2,7 +2,25 @@ import './helpers/postDate';
 import burger from './modules/burger-menu';
 
 function main() {
+    if (document.documentElement.clientWidth > 1220) {
+        $(window).scroll(function () {
+            if ($(this).scrollTop() > 375) {
+                $(".item-template-1-share-panel-content").addClass("sticky");
+            } else {
+                $(".item-template-1-share-panel-content").removeClass("sticky");
+            }
+        });
+    }
 
+    if (document.documentElement.clientWidth < 1220) {
+        $(window).scroll(function () {
+            if ($(this).scrollTop() > 305) {
+                $(".item-template-1-share-panel-content").addClass("sticky");
+            } else {
+                $(".item-template-1-share-panel-content").removeClass("sticky");
+            }
+        });
+    }
 }
 
 burger();
