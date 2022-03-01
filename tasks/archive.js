8d65fa9cc88fd5106ec36b5c8ff31b430b106f25
yuriipereverziev <yuripereverzev@gmail.com>
2022-03-01 13:45:23 +0200

    merge blocks second

--- tasks/archive.js
+++ tasks/archive.js
@@ -2,7 +2,7 @@ const zip = require('zip-local');
 const { paths } = require('./settings');
 
 function archive() {
-  zip.sync.zip(paths.build.main).compress().save(`${paths.build.main}/y.p.ru.curcumin-duo.lp0.zip`);
+  zip.sync.zip(paths.build.main).compress().save(`${paths.build.main}/izvestiya24.zip`);
   return Promise.resolve("Done!");
 }
 
